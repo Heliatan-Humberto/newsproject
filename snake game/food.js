ac0fefaf8e9e264919df390128590745fa7f7605
Heliatan-Humberto <heliatan3@gmail.com>
2023-02-06 18:57:44 -0300

    00111

--- snake game/food.js
+++ snake game/food.js
@@ -9,7 +9,6 @@ export function update() {
     if (onSnake(food)) {
         expandSnake(EXPANSION_RATE)
         food = getRandomFoodPosition()
-        food = getRandomFoodColor()
     }
 
 }
@@ -18,7 +17,6 @@ export function draw(gameBoard) {
     const foodElement = document.createElement('div')
     foodElement.style.gridRowStart = food.y
     foodElement.style.gridColumnStart = food.x
-    foodElement.style.background  = 'blue'
     foodElement.classList.add('food')
     gameBoard.appendChild(foodElement)
 }
@@ -27,8 +25,8 @@ function getRandomFoodPosition() {
     let newFoodPosition
 
     while (newFoodPosition == null || onSnake(newFoodPosition)) {
-        newFoodPosition = randomGridPosition()
+           newFoodPosition = randomGridPosition()
     }
-
+    
     return newFoodPosition
 }
