05bc2ec2b174eb8f17b3797dfd9036bb5ce1e066
Heliatan-Humberto <heliatan3@gmail.com>
2023-02-06 19:42:13 -0300

    yeahh

--- snake game/game.js
+++ snake game/game.js
@@ -11,7 +11,7 @@ let lastRenderTime = 0
 let gameOver = false
 
 requestAnimationFrame(main)
-
+// caminho do site:
 function main(currentTime) {
     if (gameOver) {
         if (confirm('You lost!')) {
